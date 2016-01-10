a6aff8839193c812c684f3ff26e194240f2a9013
yangandiris <1270968482@qq.com>
2016-01-10 21:54:32 +0800

    0110

--- Maven/src/main/java/com/yang/controller/LoginAction.java
+++ Maven/src/main/java/com/yang/controller/LoginAction.java
@@ -1,10 +1,18 @@
 package com.yang.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.yang.bean.Customer;
 import com.yang.service.LoginService;
 
 @Controller
@@ -12,9 +20,34 @@ import com.yang.service.LoginService;
 public class LoginAction {
 	@Autowired
 	private LoginService loginservice;
-	@RequestMapping(value="login")
-    public String Login(){
-    	System.out.println(loginservice.Login("yang", "yang"));
-    	return "index.jsp";
+//	@RequestMapping(value="login")
+//    public String Login(){
+//    	System.out.println(loginservice.Login("yang", "yang"));
+//    	return "success";
+//    }
+
+	@Autowired
+	private  HttpServletRequest request;
+		
+	@RequestMapping(value="save")
+    public String Save(){
+    	System.out.println("Saveuser");
+    	return "login";
     }
+	@RequestMapping(value = "saveuser") 
+    @ResponseBody  
+    public void saveUser(@RequestBody List<Customer> users) { 
+		System.out.println(123);
+        System.out.println(users.get(0).getUsername());
+		//return users; 
+    } 
+	
+	@RequestMapping(value = "setcode")
+	@ResponseBody 
+    public String setCode(String yzmcode) { 
+		System.out.println(yzmcode);
+		request.getSession().setAttribute("code", yzmcode);
+		String X=yzmcode;
+		return X;
+    } 
 }
--- Maven/src/main/webapp/index.jsp
+++ Maven/src/main/webapp/index.jsp
@@ -1,5 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+<script type="text/javascript">
+
+</script>
+<script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+</head>
 <body>
-<h2>Hello World!</h2>
+<script type="text/javascript">  
+    $(document).ready(function(){  
+        var saveDataAry=[];  
+        var data1={"id":"1","username":"test","password":"gz"};  
+        var data2={"id":"2","username":"ququ","password":"gr"};  
+        saveDataAry.push(data1);  
+        saveDataAry.push(data2);         
+        $.ajax({ 
+            type:"POST", 
+            url:"saveuser", 
+            dataType:"json",      
+            contentType:"application/json",               
+            data:JSON.stringify(saveDataAry), 
+            success:function(data){ 
+                   alert(data)                    
+            } 
+         }); 
+    });  
+</script>
 </body>
 </html>
